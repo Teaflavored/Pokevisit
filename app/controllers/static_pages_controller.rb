0cbf848313355b68a9cbb04510d1eb2670d910c2
Auster Chen <Auster.s.chen@gmail.com>
2014-11-24 13:37:57 -0800

    navbar transparent

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def landing_page
+    render :landing_page
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+
+  root to: "static_pages#landing_page"
 end
