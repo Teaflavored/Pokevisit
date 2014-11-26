da906f20105726aa05c61443c3cbcf7093859fc4
Auster Chen <Auster.s.chen@gmail.com>
2014-11-26 10:11:56 -0800

    added input fields for date

--- app/assets/javascripts/views/filters/date_filter.js
+++ app/assets/javascripts/views/filters/date_filter.js
@@ -3,10 +3,23 @@ Pokevisit.Views.DateFilter = Backbone.CompositeView.extend({
 
   className: "date-filter filter",
 
+
+  attachCalendars: function(){
+    this.$("#check-in-filter").datepicker({
+      onRender: function(date) {
+        return date.valueOf() < now.valueOf() ? 'disabled' : '';
+      }
+    })
+
+  },
+
   render: function(){
     var renderedContent = this.template();
     this.$el.html(renderedContent)
 
+    setTimeout(function(){
+      this.attachCalendars();
+    }.bind(this), 0)
     return this;
   }
 })
