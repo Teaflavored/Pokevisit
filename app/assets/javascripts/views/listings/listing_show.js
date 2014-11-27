83c379854dfda7a5c1caaa1c70075f80098f53f6
Auster Chen <Auster.s.chen@gmail.com>
2014-11-26 19:17:47 -0800

    added listing show page

--- app/assets/javascripts/views/listings/listing_show.js
+++ app/assets/javascripts/views/listings/listing_show.js
@@ -1,17 +1,23 @@
 Pokevisit.Views.ListingShow = Backbone.CompositeView.extend({
   template: JST["listings/show"],
-  
+
   initialize: function(options){
     this.listenTo(this.model, "sync", this.render)
   },
 
+  attachCarousel: function(){
+  },
+
   render: function(){
     var renderedContent = this.template({
-      listing: this.model
+      listing: this.model,
+      images: this.model.images()
     })
-    this.$el.html(renderedContent)
 
-    this.attachSubviews()
+    this.$el.html(renderedContent);
+    setTimeout(function(){
+      this.attachCarousel();
+    }.bind(this), 0)
 
     return this;
   }
