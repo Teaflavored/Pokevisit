4606537e3fd882c0adeea7e9a7f4c1ad5df561cb
Auster Chen <Auster.s.chen@gmail.com>
2014-11-29 20:41:15 -0800

    added everything that needs to be on new form

--- app/assets/javascripts/views/listings/listing_new.js
+++ app/assets/javascripts/views/listings/listing_new.js
@@ -1,7 +1,27 @@
 Pokevisit.Views.ListingNew = Backbone.CompositeView.extend({
   template: JST["listings/new"],
 
+  className:"listing-new",
+
+  initialize: function(options){
+    //once this page is submitted, on success of creation, should link user to show page
+    //of the listing he/she just made and let them edit the summary etc which can be show on other
+    //pages
+    this.setFilepicker();
+  },
+
+
+
+  setFilepicker: function(){
+    if (filepicker){
+      //if loaded set key
+      filepicker.setKey("AJXpnUispSeOzkYcpSa8Kz");
+    }
+  },
+
   render: function(){
+    this.setFilepicker();
+    //make sure filepicker key is set
     var renderedContent = this.template()
     this.$el.html(renderedContent)
 
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,9 +11,25 @@ class ListingsController < ApplicationController
     render :show
   end
 
+  def create
+    @listing = current_user.listings.new(listing_params)
+    if @listing.save
+      render :show
+    else
+      render json: @listing.errors.full_messages, status: 422
+    end
+  end
+
   def own_listings
     #get list of user's own listings then ride off of index json
+    #use for approve and deny
     @listings = current_user.listings
     render :index
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:hometype, :roomtype, :accomodates, :date_avail, :date_end, :address, :price)
   end
 end
