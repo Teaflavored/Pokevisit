a45b57ed41f5682044f00850573947c37321d321
Auster Chen <Auster.s.chen@gmail.com>
2014-12-01 10:17:08 -0800

    added listing summary information

--- app/assets/javascripts/views/listings/listing_about.js
+++ app/assets/javascripts/views/listings/listing_about.js
@@ -1,3 +1,10 @@
 Pokevisit.Views.ListingAbout = Backbone.CompositeView.extend({
-  
+  template: JST["listings/about"],
+  //need to account if current user is viewing this part
+  render: function(){
+    var renderedContent = this.template()
+    this.$el.html(renderedContent)
+
+    return this;
+  }
 })
