ba7bbcd4f57511774df635c02c1141053216d706
Auster Chen <Auster.s.chen@gmail.com>
2014-12-03 13:22:13 -0800

    added guest login along with automated typing

--- app/assets/javascripts/user_form.js
+++ app/assets/javascripts/user_form.js
@@ -2,11 +2,43 @@ $(function(){
   var $form_modal = $('.cd-user-modal');
   var $form_login = $form_modal.find('#cd-login');
   var $form_signup = $form_modal.find('#cd-signup');
+  var $signInButton = $form_modal.find("#main-modal-login")
+  var $guestButton = $form_modal.find("#main-modal-guest-login")
+
+  $guestButton.on("click", function(event){
+    event.preventDefault();
+
+    var string = "pikachu@pokemon.io";
+    var pwstring = "password"
+    var index = 0;
+    var pwindex = 0;
+
+    var printChar = function(str, pwstr){
+
+      if (index > str.length && pwindex > pwstring.length){
+        $("#main-modal-login").click();
+        return
+      }
+      setTimeout(function(){
+        $("#signin-email").val(str.slice(0, index))
+        $("#signin-password").val(pwstr.slice(0, pwindex))
+        index++
+        pwindex++
+        printChar(str, pwstr)
+      },25)
+    }
+    printChar(string, pwstring)
+
+
+    // $signInButton.click();
+  })
+
 
   $form_login.on('submit', function(event){
     event.preventDefault();
     var queryString = $("#main-modal-login").data("query")
     var userParams = $form_login.find("form").serializeJSON();
+
     $.ajax({
       url: "/session",
       type: "POST",
@@ -40,4 +72,9 @@ $(function(){
       }
     })
   });
+
+
+
+
+  //need to get guest button working
 })
