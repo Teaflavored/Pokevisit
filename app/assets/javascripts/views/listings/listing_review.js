125b98ddd641d9b79eb0fccaff111a66703b842a
Auster Chen <Auster.s.chen@gmail.com>
2014-12-03 15:57:09 -0800

    added reviews section

--- app/assets/javascripts/views/listings/listing_review.js
+++ app/assets/javascripts/views/listings/listing_review.js
@@ -1,3 +1,13 @@
 Pokevisit.Views.ListingReview = Backbone.CompositeView.extend({
+  template: JST["listings/review"],
 
+  render: function(){
+    var renderedContent = this.template({
+      listing: this.model
+    })
+
+    this.$el.html(renderedContent)
+
+    return this;
+  }
 })
