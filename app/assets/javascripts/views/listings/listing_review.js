4c3385b3d653712139361e8cc1cf18183ef74aef
Auster Chen <Auster.s.chen@gmail.com>
2014-12-03 21:01:38 -0800

    added button and creation of reviews

--- app/assets/javascripts/views/listings/listing_review.js
+++ app/assets/javascripts/views/listings/listing_review.js
@@ -1,6 +1,11 @@
 Pokevisit.Views.ListingReview = Backbone.CompositeView.extend({
   template: JST["listings/review"],
 
+  events: {
+    "keyup textarea.new-listing-review-text": "updateText",
+    "click button#create-review": "createReview",
+  },
+
   initialize: function(){
     this.reviewsSelector = "div.listing-review-content"
     this.listenTo(this.model, "sync", this.render)
@@ -11,6 +16,26 @@ Pokevisit.Views.ListingReview = Backbone.CompositeView.extend({
       this.addView(review)
     }.bind(this))
 
+    this._reviewParams = {
+      "review": {
+        "listing_id": this.model.id,
+        "review_text": null,
+        "rating": null
+      }
+    }
+
+  },
+
+  createReview: function(event){
+    event.preventDefault();
+    var newReview = new Pokevisit.Models.Review();
+    newReview.set(this._reviewParams);
+    newReview.save({}, {
+      success: function(){
+        this.model.reviews().add(newReview);
+        this.$("#create-review").attr("disabled", true)
+      }.bind(this)
+    })
   },
 
   addView: function(review){
@@ -28,17 +53,42 @@ Pokevisit.Views.ListingReview = Backbone.CompositeView.extend({
     }.bind(this))
   },
 
+  attachRating: function(){
+    this.$("#new-listing-rating").raty({
+      half: true,
+      click: function(score, event){
+        this._reviewParams.review.rating = score;
+        this.updateButton();
+      }.bind(this)
+    })
+  },
+
+  updateButton: function(){
+    var $button = this.$("#create-review")
+    if (this._reviewParams.review.listing_id && this._reviewParams.review.rating &&
+        this._reviewParams.review.review_text && this._reviewParams.review.review_text.length > 0){
+      $button.removeAttr("disabled")
+    } else {
+      $button.attr("disabled", true)
+    }
+  },
+
+  updateText: function(event){
+    this._reviewParams.review.review_text = $(event.currentTarget).val();
+    this.updateButton();
+  },
+
   render: function(){
     var renderedContent = this.template({
       listing: this.model
     })
-    this.$el.html(renderedContent)
-    this.attachSubviews()
+    this.$el.html(renderedContent);
+    this.attachSubviews();
 
     setTimeout(function(){
       //rating
-      this.$("#new-listing-rating").raty()
-    },0)
+      this.attachRating()
+    }.bind(this),0)
 
     return this;
   }
