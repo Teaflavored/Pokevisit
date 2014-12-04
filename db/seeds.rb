a51b2d123cf65652dc16109ae3abfbb6254b9918
Auster Chen <Auster.s.chen@gmail.com>
2014-12-04 11:58:52 -0800

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,6 +41,7 @@ Listing.transaction do
 
   ash.listings.first.reviews.create(user: brock, rating:3.3, review_text: "It was an enjoyable stay, ash really treated us well here. Every thing was just as we expected, except there was no Nurse Joy!")
   ash.listings.first.reviews.create(user: misty, rating:4, review_text: "I loved the inn, I'll probably try to find time to come back here again.")
+  sleep(1)
   ash.listings.first.update_score
 
 
@@ -56,6 +57,7 @@ Listing.transaction do
   brock.listings.first.listing_images.create(url: "/assets/city/city_2.jpg")
   brock.listings.first.reviews.create(user: oak, rating:1.5, review_text: "Bad city vibe, brock misled us on this place!")
   brock.listings.first.reviews.create(user: gary, rating:1, review_text: "Came here with grandpa, I found this place to be terrible. Way too many wild pokemon running amok. Not worth the money.")
+  sleep(1)
   brock.listings.first.update_score
 
   misty.listings.create(roomtype: "water",
@@ -69,6 +71,7 @@ Listing.transaction do
 
   misty.listings.first.listing_images.create(url: "/assets/sea/sea_1.jpg")
   misty.listings.first.reviews.create(user: ash, rating:4.3, review_text: "Beautiful place, I got to see many water pokemon. I might even become a fan myself now!")
+  sleep(1)
   misty.listings.first.update_score
 
   oak.listings.create(roomtype: "plains",
@@ -85,6 +88,7 @@ Listing.transaction do
   oak.listings.first.reviews.create(user: ash, rating: 3, review_text: "Average place, I've seen many similar areas during my adventures, it's just like Route whatever!")
   oak.listings.first.reviews.create(user: brock, rating: 3.5, review_text: "Had an enjoyable time here, I saw a few cool pokemon, and not to mention we also bumped into Nurse Joy!!")
   oak.listings.first.reviews.create(user: misty, rating: 3.8, review_text: "Not a bad place, I prefer the sea much better though. The plains are kind of boring, but it was beautiful.")
+  sleep(1)
   oak.listings.first.update_score
 
   gary.listings.create(roomtype: "city",
@@ -97,6 +101,7 @@ Listing.transaction do
                        description: "Luxurious castle because you deserve it for catching them all!")
   gary.listings.first.listing_images.create(url: "/assets/castle/castle_1.png")
   gary.listings.first.reviews.create(user: ash, rating: 4, review_text: "I actually quite enjoyed the stay at my rival's castle. I mean I'm not jealous or anything, but why does he have a castle! I won the Championship!!")
+  sleep(1)
   gary.listings.first.update_score
 
   brock.listings.create(roomtype: "plains",
@@ -111,6 +116,7 @@ Listing.transaction do
   brock.listings.second.listing_images.create(url: "/assets/viridian_forest/viridian_2.jpg")
   brock.listings.first.reviews.create(user: ash, rating: 5, review_text: "I remember viridian forest like it was yesterday, I caught my best friend pikachu here.")
   brock.listings.first.reviews.create(user: gary, rating:3, review_text: "Good place, I love coming here to train my pokemon, it's pretty enjoyable.")
+  sleep(1)
   brock.listings.first.update_score
 
 
@@ -130,6 +136,16 @@ Listing.transaction do
   sleep(1)
   misty.listings.second.update_score
 
+  misty.listings.create(roomtype: "water",
+                        hometype: "sea",
+                        address: "San Mateo, CA",
+                        date_avail: Date.new(2014, 12, 3),
+                        date_end: Date.new(2020, 1, 1),
+                        price: 559,
+                        accomodates: 5,
+                        description: "This sea area has water so clear you can see yourself")
+  misty.listings.third.listing_images.create(url: "/assets/sea/clear_water.jpg")
+
   ash.listings.create(roomtype: "plains",
                       hometype: "land",
                       address: "3420 Judah St, San Francisco, CA, 94122",
@@ -142,6 +158,7 @@ Listing.transaction do
   ash.listings.second.listing_images.create(url: "/assets/plains/plains_4.jpg")
   ash.listings.second.listing_images.create(url: "/assets/plains/plains_5.jpg")
   ash.listings.second.reviews.create(user: brock, rating:2, review_text: "Probably one of the most boring places I've stayed at, no girls (Nurse Joy). I want a refund.")
+  sleep(1)
   ash.listings.second.update_score
 
   ash.listings.create(roomtype: "water",
@@ -156,6 +173,7 @@ Listing.transaction do
   ash.listings.third.reviews.create(user: oak, rating:4, review_text: "Was not disappointed, this place was just as advertised. I had a great time seeing all the underwater pokemon.")
   ash.listings.third.reviews.create(user: brock, rating: 5, review_text: "Best place, we happened to bump into Nurse Joy here! Loved seeing the pokemon as well. Beautiful place!")
   ash.listings.third.reviews.create(user: misty, rating: 5, review_text: "Since I am a fan of water-type pokemon, this place was a definite go-to place for me. I'll probably take more vacations in this area.")
+  sleep(1)
   ash.listings.third.update_score
 
 
@@ -169,6 +187,7 @@ Listing.transaction do
                           description: "Come for a stay in the city, You'll enjoy it! Pika Pika~")
   pikachu.listings.first.listing_images.create(url: "/assets/light_city/light_city_1.jpg")
   pikachu.listings.first.reviews.create(user: ash, rating:4, review_text: "Great city, it's nice to just stay in civilization, away from the pokemon for awhile.")
+  sleep(1)
   pikachu.listings.first.update_score
 
   pikachu.listings.create(roomtype: "plains",
@@ -192,6 +211,30 @@ Listing.transaction do
                           description: "Legendary place where Lugia supposedly dwells"
                           )
   pikachu.listings.third.listing_images.create(url: "/assets/lugia/lugia_cave_1.jpg")
+
+  pikachu.listings.create(roomtype:"city",
+                          hometype: "land",
+                          address: "Palo Alto, CA",
+                          date_avail: Date.new(2014, 12, 3),
+                          date_end: Date.new(2020,1,1),
+                          price:333,
+                          accomodates:7,
+                          description: "City for all the night-dwellers"
+                          )
+  pikachu.listings.find_by(address: "Palo Alto, CA").listing_images.create(url: "/assets/city/tokyo_1.jpg")
+  pikachu.listings.find_by(address: "Palo Alto, CA").listing_images.create(url: "/assets/city/tokyo_2.jpg")
+
+  pikachu.listings.create(roomtype:"city",
+                          hometype: "land",
+                          address: "San Mateo, CA",
+                          date_avail: Date.new(2014, 12, 3),
+                          date_end: Date.new(2020,1,1),
+                          price:123,
+                          accomodates:7,
+                          description: "Rare sky for all you people that love flying pokemon!"
+                          )
+  pikachu.listings.find_by(description: "Rare sky for all you people that love flying pokemon!").listing_images.create(url: "/assets/sky/sky_1.jpg")
+  pikachu.listings.find_by(description: "Rare sky for all you people that love flying pokemon!").listing_images.create(url: "/assets/sky/sky_2.jpg")
 
 end
 
